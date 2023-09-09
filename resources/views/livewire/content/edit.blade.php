9c20a8e2897fed2c7ea298526ff29e47e63742cc
marcionavarro <marcionavarrodearaujo@gmail.com>
2023-09-08 23:19:54 -0300

    19-Conhecendo o Tailwind

--- resources/views/livewire/content/edit.blade.php
+++ resources/views/livewire/content/edit.blade.php
@@ -1,9 +1,9 @@
-<div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+<div class="max-w-7xl mx-auto mt-10 py-6 px-4 sm:px-6 lg:px-8">
 
     <x-slot name="header">Editar conteúdo</x-slot>
 
     @if (session()->has('success'))
-    <div class="wfull text-white my-3 p-4 border border-green-500 bg-green-400">
+    <div class="wfull text-white my-3 p-4 border border-green-500 bg-green-400 mb-10">
         {{ session('success') }}
     </div>
     @endif
@@ -11,17 +11,17 @@
     <form action="" wire:submit.prevent='editContent'>
         <div class="mb-5">
             <label class="block">Titulo</label>
-            <input type="text" wire:model.defer='content.title' />
+            <input type="text" wire:model.defer='content.title' class="w-full @error('content.title') border-red-700 @enderror"/>
             @error('content.title')
-            <strong>{{ $message }}</strong>
+            <strong class="block mt-2 text-red-700 font-bold">{{ $message }}</strong>
             @enderror
         </div>
 
         <div class="mb-5">
             <label class="block">Conteudo</label>
-            <input type="text" wire:model.defer='content.body' />
+            <input type="text" wire:model.defer='content.body' class="w-full @error('content.body') border-red-700 @enderror"/>
             @error('content.body')
-            <strong>{{ $message }}</strong>
+            <strong class="block mt-2 text-red-700 font-bold">{{ $message }}</strong>
             @enderror
         </div>
 
--- resources/views/livewire/content/index.blade.php
+++ resources/views/livewire/content/index.blade.php
@@ -1,23 +1,55 @@
-<div>
-    {{-- @livewire('content.content', compact('content'), key($content->id)) --}}
-    {{-- <livewire:content.content :content="$content" :key="$content->id"></livewire:content.content> --}}
-   
+<div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+    {{-- @livewire('content.content', ['content' => $content], key($content->id)) --}}
+    {{-- Laravel 7+<livewire:content.content :content="$content" :key="$content->id"></livewire:content.content> --}}
+
+
+    {{-- <h3>Testando Responsive Tailwind: </h3>--}}
+    {{-- <div class="w-1/4 h-1/4 bg-blue-300 text-white sm:bg-blue-900 md:bg-green-500--}}
+{{--                lg:bg-red-800 xl:bg-yellow-600 2xl:bg-black">--}}
+        {{-- Conteúdo exemplo...--}}
+        {{-- </div>--}}
+
     <x-slot name="header">Conteúdos Cadastrados</x-slot>
 
-    <div class="block">
-        <a href="{{ route('content.create') }}" class="text-white bg-green-600 px-4 py-2">Criar conteúdo</a>
+    <div class="w-full py-4 flex justify-end">
+        <a href="{{route('content.create')}}" class="btn btn-success">Criar Conteúdo</a>
     </div>
 
-    @if (session()->has('success'))
-        <div class="w-full px-2 py-4 border border-green-500 bg-green-400 text-white rounded">
-            {{ session('success') }}
-        </div>
+    @if(session()->has('success'))
+    <div class="w-full mb-5 px-2 py-4 border border-green-500 bg-green-400 text-white rounded">
+        {{session('success')}}
+    </div>
     @endif
 
-    @forelse ($contents as $content)
-        <livewire:content.content :content="$content" :key="$content->id"></livewire:content.content>
-    @empty
-    @endforelse
+    <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg mb-10">
+        <table class="min-w-full divide-y divide-gray-200">
+            <thead class="bg-gray-50">
+                <tr>
+                    <th scope="col"
+                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        #
+                    </th>
+                    <th scope="col"
+                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        Title
+                    </th>
+                    <th scope="col"
+                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        Criado em
+                    </th>
+                    <th scope="col"
+                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        Ação
+                    </th>
+                </tr>
+            </thead>
+            <tbody class="bg-white divide-y divide-gray-200">
+                @foreach($contents as $content)
+                <livewire:content.content :content="$content" :key="$content->id"></livewire:content.content>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 
-    {{ $contents->links() }}
+    {{$contents->links()}}
 </div>
