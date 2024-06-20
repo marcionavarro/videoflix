ad16c717b628dea509e2760672fe284ea6057fd6
marcionavarro <marcionavarrodearaujo@gmail.com>
2024-06-20 13:38:54 -0300

    25-Assinatura com Laravel Cashier Stripe - Entendendo a cobrança da assinatura, Realizando a cobrança(Incompleto)

--- app/Http/Livewire/Subscriptions/Checkout.php
+++ app/Http/Livewire/Subscriptions/Checkout.php
@@ -2,12 +2,31 @@
 
 namespace App\Http\Livewire\Subscriptions;
 
+use App\Models\User;
 use Livewire\Component;
 
 class Checkout extends Component
 {
+    protected $listeners = ['charge'];
+
+    public function charge($paymentMethodId)
+    {
+        //dd($paymentMethodId);
+
+        $user = User::find(1);
+
+        if(!$user->subscribed('default'))
+            $user->newSubscription('default', 'price_1PTZP4HwsQpGdjKDadQ2X8sh')->create($paymentMethodId);
+
+        return redirect()->route('dashboard');
+    }
+
     public function render()
     {
-        return view('livewire.subscriptions.checkout')->layout('layouts.guest');
+        $user = User::find(1);
+
+        return view('livewire.subscriptions.checkout')
+            ->with('intent', $user->createSetupIntent())
+            ->layout('layouts.guest');
     }
 }
--- readmes/25-Assinatura com Laravel Cashier Stripe.md
+++ readmes/25-Assinatura com Laravel Cashier Stripe.md
@@ -24,3 +24,13 @@ CASHIER_LOGGER=stack
 ### 288 - Component Checkout
 
 `php artisan make:livewire Subscriptions/Checkout`
+
+### 289 - Entendendo a cobrança da assinatura
+
+[assinatura-com-laravel-cashier-stripe](https://gist.github.com/NandoKstroNet/1d217a3ee99d7f8f78c6ec3723dbe7e4)
+
+### 290 - Realizando a Cobrança
+
+Numero do Cartão de crédito Teste
+
+`4111 1111 1111 1111 1230 123 65056`
